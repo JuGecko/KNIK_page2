8b804258acae9a44d8fe3d3625f12f0a474b8eea
Julia Grześkowiak <79307611+JuGecko@users.noreply.github.com>
2023-08-19 14:21:45 +0200

    last one: white breaks + margin on the 1st section

--- js/navbar_toggle.js
+++ js/navbar_toggle.js
@@ -12,17 +12,3 @@ navbarToggle.addEventListener("click", toggleNavbarVisibility);
 
 navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
 navbarMenu.addEventListener("click", toggleNavbarVisibility);
-
-
-/* test
-
-var navbarmenu = document.getElementById("navbar-menu");
-
-function w3_open() {
-  if (navbarmenu.style.display === 'block') {
-    navbarmenu.style.display = 'none';
-  } else {
-    navbarmenu.style.display = 'block';
-  }
-}
-*/
